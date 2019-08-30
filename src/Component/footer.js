c96ccd41de813199649a78e6f549390d021ad382
rijaprincy <rija0princy@gmail.com>
2019-08-30 14:09:23 +0300

    hgh

--- src/Component/footer.js
+++ src/Component/footer.js
@@ -15,7 +15,7 @@ export default class footer extends Component {
 
                             <div class="col-md-6 mt-md-0 mt-3">
 
-                                <h5 class="text-uppercase font-weight-bold">Footer text 1</h5>
+                                <h5 class="text-uppercase font-weight-bold">Facebook</h5>
                                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil
                                   repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae
                                     harum esse fugiat. Itaque, culpa?</p>
@@ -28,7 +28,7 @@ export default class footer extends Component {
                             <div class="col-md-6 mb-md-0 mb-3">
 
 
-                                <h5 class="text-uppercase font-weight-bold">Footer text 2</h5>
+                                <h5 class="text-uppercase font-weight-bold">Email</h5>
                                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum
                                   commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id
                                     excepturi hic.</p>
@@ -44,7 +44,7 @@ export default class footer extends Component {
 
 
                     <div class="footer-copyright text-center py-3">© 2018 Copyright:
-  <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+                    <a href="https://mdbootstrap.com/education/bootstrap/">Mon chez moi</a>
                     </div>
 
 
